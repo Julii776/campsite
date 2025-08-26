4b303b299c54322b86318a6366c102cf65141bda
Prakash Poudel <prakash@truemark.com.np>
2025-08-26 22:05:50 +0545

    Update s3, vapid config

--- api/config/initializers/aws.rb
+++ api/config/initializers/aws.rb
@@ -1,11 +1,13 @@
 # frozen_string_literal: true
 
-Aws.config.update({
-  region: "us-east-1",
+Aws.config.update(
+  region: Rails.application.credentials.dig(:aws, :region),
   credentials: Aws::Credentials.new(
-    Rails.application.credentials&.dig(:aws, :access_key_id),
-    Rails.application.credentials&.dig(:aws, :secret_access_key),
+    Rails.application.credentials.dig(:aws, :access_key_id),
+    Rails.application.credentials.dig(:aws, :secret_access_key)
   ),
-})
+  endpoint: Rails.application.credentials.dig(:aws, :endpoint),
+  force_path_style: Rails.application.credentials.dig(:aws, :force_path_style) || false
+)
 
 S3_BUCKET = Aws::S3::Resource.new.bucket(Rails.application.credentials&.dig(:aws, :s3_bucket) || "")
